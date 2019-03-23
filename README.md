d1f3ae34c935262b34a9eb9c75fb583f6a93bb4c
Daniel Pedro Sanchez Luyo <dnlsnchz4@gmail.com>
2019-03-22 20:10:11 -0500

    Agregamos eventos 'tap, soubletap, swipe, rotate' -- Sesion 2

--- README.md
+++ README.md
@@ -11,4 +11,9 @@
 In your browser, open the file:
 
     /www/index.html
+Session 1
+   Se realiza un primer commit al hacer click en la pantalla utilizando addEventListener
+   Se realiza un segundo commit utilizando FastClick
 
+Session 2
+  Para detectar todos los gestos, utilizaremos Hamer.js
--- www/js/gestures.js
+++ www/js/gestures.js
@@ -1,9 +1,28 @@
 var app={
     inicio:function(){
-        var botonClaro = document.querySelector("#claro");
-        var botonOscuro = document.querySelector("#oscuro");
-        botonClaro.addEventListener('click',this.ponloClaro,false);
-        botonOscuro.addEventListener('click',this.ponloOscuro,false);
+        this.iniciaBotones();
+        this.iniciaFastClick();
+        this.iniciaHammer();
+    },
+    iniciaFastClick:function(){
+        FastClick.attach(document.body);
+    },
+    iniciaBotones:function(){
+        var botonClaro = document.querySelector('#claro');
+        var botonOscuro = document.querySelector('#oscuro');
+        botonClaro.addEventListener('click',this.ponloClaro,false) ;
+        botonOscuro.addEventListener('click',app.ponloOscuro,false);
+    },
+    iniciaHammer:function(){
+        var zona = document.getElementById('zona-gestos');
+        var hammertime = new Hammer(zona);
+
+        hammertime.get('pinch').set({enable:true});
+        hammertime.get('rotate').set({enable:true});
+        //tap doubletap pan swipe pinch rotate
+        hammertime.on('tap doubletap swipe rotate',function(ev){
+            document.querySelector('#info').innerHTML=ev.type+'!';
+        });
     },
     ponloClaro:function(){
         document.body.className='claro';
